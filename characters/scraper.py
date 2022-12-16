6490b259b50f788e7cb2d0b9fa2cf1cf19caec15
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-12-16 13:32:21 +0200

    Added scraping of characters from rickandmortyapi.com

---
+++ characters/scraper.py
@@ -0,0 +1,37 @@
+import requests
+
+from characters.models import Character
+from rick_and_morty_api import settings
+
+
+def scrape_characters() -> list[Character]:
+    next_url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
+
+    characters = []
+
+    while next_url_to_scrape is not None:
+        characters_response = requests.get(next_url_to_scrape).json()
+
+        for character_dict in characters_response["results"]:
+            characters.append(
+                Character(
+                    api_id=character_dict["id"],
+                    name=character_dict["name"],
+                    status=character_dict["status"],
+                    species=character_dict["species"],
+                    gender=character_dict["gender"],
+                    image=character_dict["image"]
+                )
+            )
+
+        next_url_to_scrape = characters_response["info"]["next"]
+
+    return characters
+
+def save_characters(characters: list[Character]) -> None:
+    Character.objects.bulk_create(characters)
+
+
+def scrape_and_save_characters() -> None:
+    characters = scrape_characters()
+    save_characters(characters)
