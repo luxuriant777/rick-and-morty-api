a75b2a2d6d3cd15d017c073b05b643859d16ab54
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-12-18 12:25:10 +0200

    Added documentation using 'drf spectacular'

--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.db.models import QuerySet
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import status, generics
 from rest_framework.request import Request
 from rest_framework.decorators import api_view
@@ -10,8 +11,10 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
+@extend_schema(responses={status.HTTP_200_OK: CharacterSerializer})
 @api_view(["GET"])
 def get_random_character_view(request: Request) -> Response:
+    """Get a random character from list of Rick and Morty world's characters."""
     pks = Character.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Character.objects.get(pk=random_pk)
@@ -29,3 +32,17 @@ class CharacterListView(generics.ListAPIView):
         if name:
             queryset = queryset.filter(name__icontains=name)
         return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="Filtering by name, case insensitive",
+                required=False,
+                type=str,
+            )
+        ]
+    )
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        """List characters with optional filtering by name"""
+        return super().get(request, *args, **kwargs)
